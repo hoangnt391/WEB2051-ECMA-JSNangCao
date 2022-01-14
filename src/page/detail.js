0901f04442c8604dcbd9d7c10ef56379b6c2a569
hoangnt2001 <congtuhonguyen2001lg@gmail.com>
2022-01-14 13:00:57 +0700

    edit detail.js

--- src/page/detail.js
+++ src/page/detail.js
@@ -1,15 +1,29 @@
 import { newsList } from "../data";
+import header from "../components/header";
+import footer from "../components/footer";
 
 const detailPage = {
   print(id) {
     // console.log("id in detail Page", id);
     const result = newsList.find(((post) => post.id === id));
     return /* html */`
-    <div>
-      <h1>${result.title}</h1>
-      <img src="${result.img}" alt="" />
-      <p>${result.desc}</p>
-    </div>
+    
+
+    <div class="max-w-5xl mx-auto">
+          <div class="header">
+            ${header.print()}
+          </div>
+          <div class="main">
+          <div>
+          <h1>${result.title}</h1>
+          <img src="${result.img}" alt="" />
+          <p>${result.desc}</p>
+        </div>
+          </div>
+          <div class="footer">
+            ${footer.print()}
+          </div>
+      </div>
     `;
   },
 };
